1ea904b7e74dcc123938cf62c3e978c181e78a10
Me-Illia <sq999@ukr.net>
2023-05-08 22:15:48 +0300

    Update task-09.js
    попередній код був не правильним хоча працюючим, але це є великою помилкою! Тому було замінено на дане... наче все працює ))))))

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,14 @@
-const butEl = document.querySelector(".change-color");
-const colorEl = document.querySelector(".color");
-const bodyEl = document.querySelector("body")
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-butEl.addEventListener('click', event => {
-  event = getRandomHexColor();
-  bodyEl.style.backgroundColor = event;
-  colorEl.textContent = event;
+function refreshBackgroundColor() {
+  const color = getRandomHexColor();
+  document.body.style.background = color; // стукаємось до стилю беку щоби він змінював колір за функцією рандому
+  document.querySelector('.color').textContent = color; //стукаємось до спана щоби виводило інф.кольору
+}
 
-});
+const button = document.querySelector('.change-color'); // отримуємо доступ до кнопки за класом
+button.addEventListener('click', refreshBackgroundColor); // робимо слухача кнопки при кліку викоання функції "function refreshBackgroundColor()"
