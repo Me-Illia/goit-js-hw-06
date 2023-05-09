12c041fb45e153950a8aa5410e1d7713dabf93ad
Me-Illia <sq999@ukr.net>
2023-05-09 12:57:00 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,34 @@
-const form = document.querySelector('.login-form');
+// const form = document.querySelector('.login-form');
 
-form.addEventListener('submit', event => {
+// form.addEventListener('submit', event => {
+//   event.preventDefault(); // додаємо щоби не перезавантажувалась сторінка при передачі запиту
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+//     if (!email.value || !password.value) {
+//       alert('You have to fill form');
+//       return;
+//   }
+//   console.log(`{Login: ${email.value}, Password: ${password.value}}`);
+//     // formLogin.reset(); // малось на увазі це зробити?
+//   event.currentTarget.reset()
+// });
+const formLogin = document.querySelector('.login-form');
+
+formLogin.addEventListener('submit', event => {
   event.preventDefault();
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
-    if (!email.value || !password.value) {
-      alert('You have to fill form');
-      return;
+  const formData = {};  // ПРОПУСТИВ УМОВУ СТУКАТИСЬ ТАКИМ МЕТОДОМ — element
+  for (const element of formLogin.elements) {
+    if (element.name) {
+      formData[element.name] = element.value;
+    }
   }
-  console.log(`{Login: ${email.value}, Password: ${password.value}}`);
-    
-  event.currentTarget.reset()
+  if (formData.email && formData.password) {
+    console.log(formData);
+    formLogin.reset();
+  } else {
+    alert('You have to fill form');
+  }
 });
+
+// ПЕРЕПИСАВ ПО ІНШОМУ 
